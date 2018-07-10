000b7aab58dde55e8409fa0016ca2144b073f068
Asif Nawaz <asif.saho@gmail.com>
2018-07-10 21:30:04 +0200

    key removed and readme updated

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,7 +1,7 @@
 const appConfig = {
-    clientId: '37e1266222ea598ab2ef',
-    clientSecret: "a00382fe21cc042f631c516e3c084dfb5ae07b43",
-    dataEndpoint: "https://api.github.com/graphql",
+    clientId: '',
+    clientSecret: '',
+    dataEndpoint: 'https://api.github.com/graphql',
     OAuthEndPoint: 'https://github.com/login/oauth/authorize',
     accessTokenEndPoint: 'https://github.com/login/oauth/access_token',
     redirectUri: 'http://localhost:3000/callback',
